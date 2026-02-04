46877eee4217c757a8479996bf64849dc5f6fadb
Leon Heschl <heschl.leon@gmail.com>
2026-02-04 15:19:03 +0100

    fixed firstIndexOf method

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,10 @@
+import request.InvalidRequest;
+import request.parser.RequestParser;
+
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class Test {
-    public static void main(String[] args) {
-        String s = "GET / HTTP/1.1";
-        String[] p = s.split("/");
-        System.out.println();
-
+    public static void main(String[] args) throws InvalidRequest {
     }
 }
--- src/request/parser/RequestParser.java
+++ src/request/parser/RequestParser.java
@@ -2,6 +2,7 @@ package request.parser;
 
 import request.InvalidRequest;
 import request.Request;
+import request.RequestLine;
 
 public class RequestParser {
 
@@ -10,33 +11,24 @@ public class RequestParser {
 //        String[] parts = head.split("\n");
 
         String stringRequest = everythingToString(requestBytes);
-        String requestLine = getRequestLine(requestBytes);
+        RequestLine requestLine = new RequestLine(getRequestLine(requestBytes)); // ignores leading whitespaces
         System.out.println(requestLine);
 
         return null;
     }
 
-    private static String getRequestLine(byte[] requestBytes) {
+    private static String getRequestLine(byte[] requestBytes) throws InvalidRequest {
         // TODO getRequestLine doesnt work (i think because of firstIndexOf)
         byte[] toFind = {AnsiConstants.CR, AnsiConstants.LF};
         int crlfIndex = firstIndexOf(requestBytes, toFind);
+        if (crlfIndex == -1) {
+            throw new InvalidRequest("invalid RequestLine");
+        }
         String requestLine = "";
         for (int i = 0; i < crlfIndex + 2; i++) {
             requestLine += (char) requestBytes[i];
         }
         return requestLine;
-    }
-
-    public static void parseRequestLine(byte[] requestBytes) {
-        int i;
-        String method = "";
-        for (i = 0; i < requestBytes.length; i++) {
-            if (i == 32) {
-                break;
-            }
-            method += (char) requestBytes[i];
-        }
-
     }
 
     public static String everythingToString(byte[] request) {
@@ -47,38 +39,18 @@ public class RequestParser {
         return out;
     }
 
-
-    public static String headToString(byte[] request) throws InvalidRequest {
-        // only ascii characters should be used in the head
-        String out = "";
-        char current = (char) request[0];
-        char previous;
-        for (int i = 0; i < request.length; i++) {
-            previous = current;
-            current = (char) request[i];
-            // TODO not sure if this will work
-            if ((previous == '\r' || previous == '\n') && current == '\n') {
-                break;
-            }
-            if (request[i] > 127) {
-                throw new InvalidRequest("non ascii characters were used in http head");
-            }
-            out += String.valueOf((char) request[i]);
-        }
-        return out;
-    }
-
     private static int firstIndexOf(byte[] arr, byte[] toSearch) {
         // TODO can maybe be done faster
-        for (int i = 0; i < arr.length - toSearch.length - 1; i += toSearch.length) {
-            boolean matches = true;
-            for (int j = i; j < i + toSearch.length; j++) {
-                if (arr[i] != arr[j]) {
-                    matches = false;
+        for (int i = 0; i < arr.length - toSearch.length + 1; i++) {
+            int matches = 0;
+            for (int j = 0; j < toSearch.length; j++) {
+                if (arr[i + j] != toSearch[j]) {
                     break;
+                } else {
+                    matches += 1;
                 }
             }
-            if (matches) {
+            if (matches == toSearch.length) {
                 return i;
             }
         }
