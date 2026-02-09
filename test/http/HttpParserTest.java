5ccb224b6492b4281d39139da812cc250b722816
Leon Heschl <heschl.leon@gmail.com>
2026-02-09 11:11:59 +0100

    added body logic

---
+++ test/http/HttpParserTest.java
@@ -0,0 +1,30 @@
+package http;
+
+import org.junit.jupiter.api.Test;
+import request.RequestParser;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static request.RequestParserTest.fileToByteArray;
+
+class HttpParserTest {
+
+    @Test
+    void getStringBody() throws IOException {
+        byte[] b = fileToByteArray("test/standardRequestWithBody1.txt");
+        RequestParser requestParser = new RequestParser(b);
+        String actualBody = requestParser.getStringBody();
+        assertEquals("hello how are you\r\n", actualBody);
+    }
+
+    @Test
+    void getRequestHeadersTest() throws IOException {
+        byte[] b = fileToByteArray("test/standardRequest.txt");
+        RequestParser requestParser = new RequestParser(b);
+        HashMap<String, String> headers = requestParser.getRequestHeaders();
+        assertEquals(headers.get("host"), "example.com");
+        assertEquals(headers.get("priority"), "u=0, i");
+    }
+}
--- test/request/RequestParserTest.java
+++ test/request/RequestParserTest.java
@@ -13,13 +13,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 
-class RequestParserTest {
+public class RequestParserTest {
+
 
     @Test
-    void initializeRequestLineTest() throws IOException {
+    void getRequestLineTest() throws IOException {
         String requestLineArgument1 = "GET / HTTP/2.1" + HttpConstants.CR + HttpConstants.LF;
         RequestParser requestParser1 = new RequestParser(fileToByteArray("test/standardRequest.txt"));
-        RequestLine requestLine1 = requestParser1.initializeRequestLine();
+        RequestLine requestLine1 = requestParser1.getRequestLine();
         assertEquals("GET", requestLine1.getMethod());
         assertEquals("/", requestLine1.getUri());
         assertEquals(2, requestLine1.getVersion().getMajorVersion());
@@ -28,7 +29,7 @@ class RequestParserTest {
 
         String requestLineArgument2 = "HEAD / HTTP/33.20" + HttpConstants.CR + HttpConstants.LF;
         RequestParser requestParser2 = new RequestParser(fileToByteArray("test/standardRequest2.txt"));
-        RequestLine requestLine2 = requestParser2.initializeRequestLine();
+        RequestLine requestLine2 = requestParser2.getRequestLine();
         assertEquals("HEAD", requestLine2.getMethod());
         assertEquals("/", requestLine2.getUri());
         assertEquals(33, requestLine2.getVersion().getMajorVersion());
@@ -36,21 +37,12 @@ class RequestParserTest {
         assertEquals(requestLineArgument2, requestLine2.toString());
     }
     @Test
-    void testInvalidRequestLineThrowsException() {
+    void InvalidRequestLineTest_ThrowsException() {
         String requestLineArgument = "GET / aösldkfj23234420";
         assertThrows(
                 InvalidRequest.class,
                 () -> new RequestParser(stringToByteArray(requestLineArgument))
         );    }
-
-    @Test
-    void initializeRequestHeadersTest() throws IOException {
-        byte[] b = fileToByteArray("test/standardRequest.txt");
-        RequestParser requestParser = new RequestParser(b);
-        HashMap<String, String> headers = requestParser.initializeRequestHeaders();
-        assertEquals(headers.get("host"), "example.com");
-        assertEquals(headers.get("priority"), "u=0, i");
-    }
 
     private static byte[] stringToByteArray(String s) {
         byte[] byteArray = new byte[s.length()];
@@ -60,7 +52,7 @@ class RequestParserTest {
         return byteArray;
     }
 
-    private static byte[] fileToByteArray(String path) throws IOException {
+    public static byte[] fileToByteArray(String path) throws IOException {
         return Files.readAllBytes(Path.of(path));
     }
 }
